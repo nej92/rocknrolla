ce8fb7d6b17ad7e18ee40450e10cd746ceb1fdb7
nej92 <emanunez26@gmail.com>
2021-08-19 12:48:33 -0300

    IGfeed

---
+++ js/instaFeed.js
@@ -0,0 +1,7 @@
+let userFeed = new Instafeed({
+    get: 'user',
+    target: "instafeed-container",
+    resolution: 'low_resolution',
+    accessToken: 'IGQVJYQnVfUnhaTURyNGZAHVENDaU1iX1ctelZArMkJzVjd5X3lydUF1YzFDSVl3MWdiUHc2aUYwbVRldkxXLUdPSXZAzME9rWk5qckNsWENyclBna3NVRWZA1MlRhMWRMazd5ZA0dBWjFzUGNPUTVma3MyQgZDZD'
+});
+userFeed.run();
