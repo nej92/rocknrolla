5b68ddb68c27afdfd2d59d74e6e8e9af09dcc597
nej92 <emanunez26@gmail.com>
2021-10-26 16:33:03 -0300

    wwd

--- js/hamburButtons.js
+++ js/hamburButtons.js
@@ -1,7 +1,7 @@
 navDespHamburImg = document.getElementById('nav-desp');
 navDespCrossImg = document.getElementById('nav-desp');
 wwd = document.getElementById('wwd');
-wwd.addEventListener('click', whatWeDo)
+
 navDespHamburImg.addEventListener('click', showMenu);
 navDespCrossImg.addEventListener('click', hideMenu);
 function showMenu() {
@@ -10,6 +10,10 @@ function showMenu() {
 function hideMenu(){
     navDespCrossImg.style =  `visibility: hidden;`
 }
-function whatWeDo(){
-    wwd.style=``
+wwd.onmousedown = function () {
+    console.log('tocado');
+}
+
+wwd.onmouseup = function () {
+    console.log('soltado');
 }
